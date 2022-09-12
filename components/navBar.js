8896cdac3c0fad86bdf78858f0a1c7f4e8536605
Collyngodlewski <collyngodlewski@gmail.com>
2022-09-12 12:23:59 -0400

    fixed mapping issue

--- components/navBar.js
+++ components/navBar.js
@@ -2,61 +2,43 @@ import { Navbar, Text, useTheme, Link } from "@nextui-org/react";
 
 const NavBar = () => {
   const { isDark } = useTheme();
-  const collapseItems = [
-    "Home",
-    "Projects",
-    "Resume",
-    "Contact",
-  ];
+  const collapseItems = [{item: "Home", link: "/"}, {item:"Projects", link: "/projects"}, {item:"Resume", link:"/resume"}, {item:"Contact", link:"/contact"},];
+
 
   return (
-
-      // <Navbar isBordered={isDark} variant="sticky">
-      //     <Navbar.Brand>
-      //     <Text>Collyn</Text>
-      //   </Navbar.Brand>
-      //   <Navbar.Content variant="underline">
-      //     <Navbar.Link isActive href="/">Home</Navbar.Link>
-      //     <Navbar.Link isActive href="/projects">Projects</Navbar.Link>
-      //     <Navbar.Link isActive href="/resume">Resume</Navbar.Link>
-      //     <Navbar.Link isActive href="/contact">Contact</Navbar.Link>
-      //   </Navbar.Content>
-      // </Navbar>
-
-<Navbar variant="sticky">
-          <Navbar.Toggle aria-label="toggle navigation" showIn="xs"/>
-          <Navbar.Brand>
-          <Text>Collyn</Text>
-        </Navbar.Brand>
-        <Navbar.Content enableCursorHighlight
-          activeColor="primary"
-          hideIn="xs"
-          variant="highlight">
-          <Navbar.Link href="/">Home</Navbar.Link>
-          <Navbar.Link href="/projects">Projects</Navbar.Link>
-          <Navbar.Link href="/resume">Resume</Navbar.Link>
-          <Navbar.Link href="/contact">Contact</Navbar.Link>
-        </Navbar.Content>
-        <Navbar.Collapse>
-        {collapseItems.map((item, index) => (
+    <Navbar isBordered={isDark} variant="sticky">
+      
+      <Navbar.Toggle aria-label="toggle navigation" showIn="xs" />
+      <Navbar.Brand>
+        <Text>Collyn Godlewski</Text>
+      </Navbar.Brand>
+      <Navbar.Content
+        enableCursorHighlight
+        activeColor="secondary"
+        hideIn="xs"
+        variant="highlight"
+      >
+        <Navbar.Link href="/">Home</Navbar.Link>
+        <Navbar.Link href="/projects">Projects</Navbar.Link>
+        <Navbar.Link href="/resume">Resume</Navbar.Link>
+        <Navbar.Link href="/contact">Contact</Navbar.Link>
+      </Navbar.Content>
+      <Navbar.Collapse>
+        {collapseItems.map(({item, link}) => (
           <Navbar.CollapseItem key={item}>
             <Link
               color="inherit"
               css={{
                 minWidth: "100%",
               }}
-              href={index}
+              href={link}
             >
               {item}
             </Link>
-      
           </Navbar.CollapseItem>
         ))}
       </Navbar.Collapse>
-      </Navbar>
-
-
-    
+    </Navbar>
   );
 };
 
