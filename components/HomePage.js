ce226f8e0dbcfb6adae61e4d2d916bd2cb1b9ab6
Collyngodlewski <collyngodlewski@gmail.com>
2022-09-12 19:20:04 -0400

    testing hydration

--- components/HomePage.js
+++ components/HomePage.js
@@ -1,26 +1,31 @@
-import { Container, Card, Row, Text, Spacer } from '@nextui-org/react';
-import React from 'react'
-import TypewriterComponent from 'typewriter-effect';
+import { Container, Card, Row, Text, Spacer } from "@nextui-org/react";
+import React from "react";
+import TypewriterComponent from "typewriter-effect";
 
 const HomePage = () => {
   return (
-    
     <Container>
-        <Spacer y={10}/>
-             
-          <Row justify="center" align="center">
-            <Text  size={24} color="black" css={{ m: 0 }}>
-            <TypewriterComponent
-  options={{
-    strings: ['Hello World'],
-    autoStart: true,
-    loop: true,
-  }}
-/>
-            </Text>
-          </Row>
+      <Spacer y={10} />
+
+      <Row justify="center" align="center">
+        <Text size={24} color="black" css={{ m: 0 }}>
+          <TypewriterComponent
+            options={{
+              strings: ["Hello World"],
+              autoStart: true,
+              loop: true,
+            }}
+          />
+        </Text>
+      </Row>
+      <Spacer y={4} />
+      <Row justify="center" align="center">
+      <Text size={28} color="black" css={{ m: 0 }}>
+        Full Stack Web Developer
+      </Text>
+      </Row>
     </Container>
-  )
-}
+  );
+};
 
 export default HomePage;
