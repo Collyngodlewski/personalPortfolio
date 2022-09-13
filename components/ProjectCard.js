e2fb49a79574ac8c4aea122a87f0f83f6a01d510
Collyngodlewski <collyngodlewski@gmail.com>
2022-09-13 17:24:20 -0400

    styled cards a bit

---
+++ components/ProjectCard.js
@@ -0,0 +1,29 @@
+import { Card, Col, Text } from "@nextui-org/react";
+
+export const ProjectCard = () => (
+  <Card
+    align="center"
+    isPressable
+    isHoverable
+    variant="bordered"
+    css={{ mw: "400px" }}
+  >
+    <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
+      <Col>
+        <Text size={12} weight="bold" transform="uppercase" color="#ffffffAA">
+          Projects
+        </Text>
+        <Text h4 color="white">
+          Here will be my projects
+        </Text>
+      </Col>
+    </Card.Header>
+    <Card.Image
+      src="https://nextui.org/images/card-example-4.jpeg"
+      objectFit="cover"
+      width="100%"
+      height={340}
+      alt="Card image background"
+    />
+  </Card>
+);
--- components/Projects.js
+++ components/Projects.js
@@ -9,150 +9,84 @@ import {
   Button,
 } from "@nextui-org/react";
 import React from "react";
+import { ProjectCard } from "./ProjectCard.js";
 
 function ProjectsPage() {
   return (
-    <Grid.Container gap={1}  display="flex"  wrap="wrap" justify="space-around">
-  
-        <Grid sm={12} md={2}>
-          <Card isPressable isHoverable  css={{ mw: "330px" }}>
-            <Card.Header>
-              <Text b>Meow 1</Text>
-            </Card.Header>
-            <Card.Divider />
-            <Card.Body css={{ py: "$10" }}>
-              <Text>This is my first project</Text>
-            </Card.Body>
-            <Card.Divider />
-            <Card.Footer>
-              <Row justify="flex-end">
-                <Button size="sm" light>
-                  Github
-                </Button>
-                <Button size="sm" color="black">
-                  Website
-                </Button>
-              </Row>
-            </Card.Footer>
-          </Card>
-        </Grid>
-        <Grid sm={12} md={2} >
-          <Card isPressable isHoverable css={{ mw: "330px" }}>
-            <Card.Header>
-              <Text b>Meow 1</Text>
-            </Card.Header>
-            <Card.Divider />
-            <Card.Body css={{ py: "$10" }}>
-              <Text>This is my second project</Text>
-            </Card.Body>
-            <Card.Divider />
-            <Card.Footer>
-              <Row justify="flex-end">
-                <Button size="sm" light>
-                  Github
-                </Button>
-                <Button size="sm" color="black">
-                  Website
-                </Button>
-              </Row>
-            </Card.Footer>
-          </Card>
-        </Grid>
-    
+    <Grid.Container gap={2} display="flex" justify="space-evenly"  wrap="wrap" >
+      <Col>
+      <Row justify="center">
+      
+      <Grid xs={12} sm={6} lg={2}>
+      <ProjectCard/>
+      </Grid>
+      <Grid xs={12} sm={6} lg={2}>
+      <ProjectCard />
+      </Grid>
+      </Row>
+      </Col>
+      <Col>
+      <Row justify="center">
+      
+      <Grid xs={12} sm={6} lg={2}>
+      <ProjectCard/>
+      </Grid>
+      <Grid xs={12} sm={6} lg={2}>
+      <ProjectCard />
+      </Grid>
+      </Row>
+      </Col>
      
-        <Grid sm={12} md={2} >
-          <Card isPressable isHoverable css={{ mw: "330px" }}>
-            <Card.Header>
-              <Text b>Meow 1</Text>
-            </Card.Header>
-            <Card.Divider />
-            <Card.Body css={{ py: "$10" }}>
-              <Text>This is my third project</Text>
-            </Card.Body>
-            <Card.Divider />
-            <Card.Footer>
-              <Row justify="flex-end">
-                <Button size="sm" light>
-                  Github
-                </Button>
-                <Button size="sm" color="black">
-                  Website
-                </Button>
-              </Row>
-            </Card.Footer>
-          </Card>
-        </Grid>
-        <Grid sm={12} md={2}>
-          <Card isPressable isHoverable css={{ mw: "330px" }}>
-            <Card.Header>
-              <Text b>Meow 1</Text>
-            </Card.Header>
-            <Card.Divider />
-            <Card.Body css={{ py: "$10" }}>
-              <Text>This is my fourth project</Text>
-            </Card.Body>
-            <Card.Divider />
-            <Card.Footer>
-              <Row justify="flex-end">
-                <Button size="sm" light>
-                  Github
-                </Button>
-                <Button size="sm" color="black">
-                  Website
-                </Button>
-              </Row>
-            </Card.Footer>
-          </Card>
-        </Grid>
-  
-     
-        <Grid sm={12} md={2}>
-          <Card isPressable isHoverable css={{ mw: "330px" }}>
-            <Card.Header>
-              <Text b>Meow 1</Text>
-            </Card.Header>
-            <Card.Divider />
-            <Card.Body css={{ py: "$10" }}>
-              <Text>This is my fifth project</Text>
-            </Card.Body>
-            <Card.Divider />
-            <Card.Footer>
-              <Row justify="flex-end">
-                <Button size="sm" light>
-                  Github
-                </Button>
-                <Button size="sm" color="black">
-                  Website
-                </Button>
-              </Row>
-            </Card.Footer>
-          </Card>
-        </Grid>
-        <Grid sm={12} md={2}>
-          <Card isPressable isHoverable css={{ mw: "330px" }}>
-            <Card.Header>
-              <Text b>Meow 1</Text>
-            </Card.Header>
-            <Card.Divider />
-            <Card.Body css={{ py: "$10" }}>
-              <Text>This is my sixth project</Text>
-            </Card.Body>
-            <Card.Divider />
-            <Card.Footer>
-              <Row justify="flex-end">
-                <Button size="sm" light>
-                  Github
-                </Button>
-                <Button size="sm" color="black">
-                  Website
-                </Button>
-              </Row>
-            </Card.Footer>
-          </Card>
-        </Grid>
-     
-     
+   
     </Grid.Container>
+    // <Container gap={0}  display="flex"  >
+    //   <Row >
+    //     <Col>
+    //       <Card isPressable isHoverable >
+    //         <Card.Header>
+    //           <Text b>Meow 1</Text>
+    //         </Card.Header>
+    //         <Card.Divider />
+    //         <Card.Body css={{ py: "$10" }}>
+    //           <Text>This is my first project</Text>
+    //         </Card.Body>
+    //         <Card.Divider />
+    //         <Card.Footer>
+    //           <Row justify="flex-end">
+    //             <Button size="sm" light>
+    //               Github
+    //             </Button>
+    //             <Button size="sm" color="black">
+    //               Website
+    //             </Button>
+    //           </Row>
+    //         </Card.Footer>
+    //       </Card>
+    //       </Col>
+    //     <Col>
+    //       <Card isPressable isHoverable >
+    //         <Card.Header>
+    //           <Text b>Meow 1</Text>
+    //         </Card.Header>
+    //         <Card.Divider />
+    //         <Card.Body css={{ py: "$10" }}>
+    //           <Text>This is my second project</Text>
+    //         </Card.Body>
+    //         <Card.Divider />
+    //         <Card.Footer>
+    //           <Row justify="flex-end">
+    //             <Button size="sm" light>
+    //               Github
+    //             </Button>
+    //             <Button size="sm" color="black">
+    //               Website
+    //             </Button>
+    //           </Row>
+    //         </Card.Footer>
+    //       </Card>
+    //       </Col>
+    //     </Row>
+  //   </Container>
   );
 }
 
