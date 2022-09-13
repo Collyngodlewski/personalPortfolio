2b04768b2b27b3a53ffa16ccb9fdb3e5b860bdf5
Collyngodlewski <collyngodlewski@gmail.com>
2022-09-12 22:57:14 -0400

    yum yum

--- components/HomePage.js
+++ components/HomePage.js
@@ -4,8 +4,6 @@ import { Container, Card, Row, Text, Spacer, Link } from "@nextui-org/react";
 import React from "react";
 import TypewriterComponent from "typewriter-effect";
 
-
-
 const HomePage = () => {
   return (
     <Container>
@@ -15,7 +13,7 @@ const HomePage = () => {
         <Text size={30} color="black" css={{ m: 0 }}>
           <TypewriterComponent
             options={{
-              strings: ["Hello World!","Full Stack Web Developer"],
+              strings: ["Hello World!", "Full Stack Web Developer."],
               autoStart: true,
               loop: true,
             }}
@@ -23,16 +21,18 @@ const HomePage = () => {
         </Text>
       </Row>
       <Spacer y={12} />
-       <Row justify="center" align="center">
-        <Link href="https://github.com/Collyngodlewski" target="_blank" >
-       <FontAwesomeIcon icon={faGithub} size="3x" color="black"/>
-       </Link>
-       <Spacer x={2} />
-       <Link href="https://www.linkedin.com/in/collyngodlewski/" target="_blank">
-        <FontAwesomeIcon icon={faLinkedin} size="3x" color="black" />
-       </Link>
-      </Row> 
-      
+      <Row justify="center" align="center">
+        <Link href="https://github.com/Collyngodlewski" target="_blank">
+          <FontAwesomeIcon icon={faGithub} size="3x" color="black" />
+        </Link>
+        <Spacer x={2} />
+        <Link
+          href="https://www.linkedin.com/in/collyngodlewski/"
+          target="_blank"
+        >
+          <FontAwesomeIcon icon={faLinkedin} size="3x" color="black" />
+        </Link>
+      </Row>
     </Container>
   );
 };
