82b3cf6a5381e6e65185f8499ff2d40667a130a3
Collyngodlewski <collyngodlewski@gmail.com>
2022-09-12 20:57:10 -0400

    added social links

--- components/HomePage.js
+++ components/HomePage.js
@@ -1,6 +1,10 @@
-import { Container, Card, Row, Text, Spacer } from "@nextui-org/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { Container, Card, Row, Text, Spacer, Link } from "@nextui-org/react";
 import React from "react";
 import TypewriterComponent from "typewriter-effect";
+
+
 
 const HomePage = () => {
   return (
@@ -18,12 +22,17 @@ const HomePage = () => {
           />
         </Text>
       </Row>
-      <Spacer y={4} />
-      {/* <Row justify="center" align="center">
-      <Text size={28} color="black" css={{ m: 0 }}>
-        Full Stack Web Developer
-      </Text>
-      </Row> */}
+      <Spacer y={12} />
+       <Row justify="center" align="center">
+        <Link href="https://github.com/Collyngodlewski" target="_blank" >
+       <FontAwesomeIcon icon={faGithub} size="3x" color="black"/>
+       </Link>
+       <Spacer x={2} />
+       <Link href="https://www.linkedin.com/in/collyngodlewski/" target="_blank">
+        <FontAwesomeIcon icon={faLinkedin} size="3x" color="black" />
+       </Link>
+      </Row> 
+      
     </Container>
   );
 };
